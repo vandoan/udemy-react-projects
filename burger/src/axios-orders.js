be1ab5a19ad88b12b82f6e1c2fce5dddd6f583c2
vandoan <vandoona@gmail.com>
2020-09-03 23:56:11 -0700

    Lesson 237

--- burger/src/axios-orders.js
+++ burger/src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-burger-88f9c.firebaseio.com/'
+    baseURL: 'https://react-burger-88f9c.firebaseio.com'
 })
 
 export default instance;
--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -3,13 +3,62 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders';
+import Input from '../../../components/UI/Input/Input';
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            city: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },,
+            zip: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },,
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                        
+                    ]
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -21,17 +70,6 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Mindy Meow',
-                address: {
-                    street: '11 Meow Lane',
-                    state: 'CA',
-                    city: 'Goleta',
-                    zip: '911111'
-                },
-                email: 'meow@time.com',
-                deliveryMethod: 'fastest'
-            }
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -46,10 +84,10 @@ class ContactData extends Component {
     render () {
         let form = (
             <form>
-                <input className={classes.Input} type='text' name='name' placeholder='Your name' />
-                <input className={classes.Input} type='email' name='emai' placeholder='your@email.com' />
-                <input className={classes.Input} type='text' name='street' placeholder='Street' />
-                <input className={classes.Input} type='email' name='postal' placeholder='Postal code' />
+                <Input inputtype='input' type='text' name='name' placeholder='Your name' />
+                <Input inputtype='input' type='email' name='emai' placeholder='your@email.com' />
+                <Input inputtype='input' type='text' name='street' placeholder='Street' />
+                <Input inputtype='input' type='email' name='postal' placeholder='Postal code' />
                 <Button btnType='Success' clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
@@ -58,7 +96,7 @@ class ContactData extends Component {
         }
         return (
             <div className={classes.ContactData}>
-                <h4>Enter our Contact Data</h4>l
+                <h4>Enter Your Contact Data</h4>
                 {form}
             </div>
         )
